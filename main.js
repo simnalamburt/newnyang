aed2e4fd5241481da70684ee73e25c1fcb19d97e
Hyeon Kim <simnalamburt@gmail.com>
2015-02-12 15:05:35 +0900

    Menu

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ angular
 .module('appname', ['ngRoute'])
 .config(function($locationProvider, $routeProvider) {
   $locationProvider.html5Mode(true);
-  $routeProvider.when('/', {
+
+  $routeProvider
+  .when('/', {
     templateUrl:'main.html',
+  })
+  .when('/about', {
+    templateUrl:'about.html',
+  })
+  .when('/news', {
+    templateUrl:'news.html',
+  })
+  .when('/contact', {
+    templateUrl:'contact.html',
   });
 })
